9f24e4ee1b22ff086115a8f6976dddfb65c3e80b
ariririri <fatal.t.h.f.flydream.bamboo@gmail.com>
2019-11-14 21:06:48 +0900

    add base polynomail action

--- examples/print_sample.rs
+++ examples/print_sample.rs
@@ -1,19 +1,42 @@
 extern crate rust_algebra;
 use rust_algebra::poly::Poly;
+use num_rational::*;
 
 fn main() {
 // ライブラリhogeの使用例
   println!("{:?}", rust_algebra::poly::Poly{ f: vec![3.0, 4.1]});
 
   let mut a = Poly{f: vec![1, 2]};
-  let mut b = Poly{f: vec![1, 2]};
+  let b = Poly{f: vec![1, 2]};
   let c = Poly{f: vec![1, 2]};
   let d = Poly{f: vec![1, 2]};
   let e = Poly{f: vec![1, 2]};
-  let mut g = a + b;
-  let h = b - d;
+  let di = Poly{f: vec![2, 1]};
+  let mut g = &a + &b;
+  let h = e - d;
+  a += b;
+  // a *= c;
   println!("{:?}", g);
   println!("{:?}", h.degree());
   println!("{:?}", g.scalar(3));
   println!("{:?}", g.substitue(3));
+  // println!("{:?}", (&a).power(3));
+  // println!("{:?}", a.comp(c));
+  println!("{:?}", di.clone() % di.clone());
+  println!("{:?}", di.clone() / di.clone());
+  println!("df {:?}", di.df());
+  let rat = Ratio::new(-1, -2);
+  let rat_2 = Ratio::new(-10, -2);
+  let rat_3 = Ratio::new(-5, -2);
+  let rat_one = Ratio::new(1, 1);
+  let rat_poly = Poly{f: vec![rat; 3]};
+  println!("start rational {:?}", rat_poly.monic());
+  let rat_zero = Ratio::new(0, 1);
+  let rat_poly_2 = Poly{f: vec![rat, rat_2, rat_3]};
+  let rat_poly_3 = Poly{f: vec![- rat_one, rat_zero, rat_zero, rat_one]};
+  let rat_poly_4 = Poly{f: vec![rat_zero, rat_one ]};
+  println!("{:?}", rat_poly.monic());
+  println!("{:?}", rat_poly.gcd(&rat_poly_2));
+  println!("{:?}", rat_poly_3.gcd(&rat_poly));
+  println!("{:?}", rat_poly_4.squarefree());
 }
--- src/poly.rs
+++ src/poly.rs
@@ -1,10 +1,31 @@
 use num_traits::zero;
+use num_traits::one;
 use num_traits::Zero;
 use std::cmp;
 use std::fmt;
-use std::ops::{Add, Sub, Mul};
+use std::ops::{Add, Sub, Mul, Div, Rem};
+use std::ops::{AddAssign, MulAssign};
 use num_traits::Num;
+use num_rational::Rational;
 
+
+
+pub trait Coeff: 
+  Num 
+ +std::clone::Clone
+ +AddAssign
+{
+    fn a(self) ;
+}
+
+impl Coeff for i32 {
+    fn a(self) {
+        print!("{:?}", self);
+    }
+
+}
+
+#[derive(Clone)]
 pub struct Poly<T: Num> {
     pub f: Vec<T>,
 }
@@ -58,24 +79,44 @@ impl<T: std::string::ToString +Num> fmt::Debug for Poly<T> {
     }
 }
 
-impl<T: Add+Num+std::clone::Clone> Add for Poly<T> {
-    type Output = Self;
+impl<T: Num+std::clone::Clone+AddAssign> AddAssign for Poly<T> {
+    fn add_assign(&mut self, other: Self) {
+        let max_dim = cmp::max(self.degree(), other.degree());
+        for _ in 0..max_dim-self.degree(){ (*self).f.push(zero())};
+        for i in 0..other.degree()+1 { (*self).f[i] += other.f[i].clone() }
+    }
+}
 
+impl<T: Num+std::clone::Clone+AddAssign> Add for &Poly<T> {
+    type Output = Poly<T>;
+    fn add(self, other: Self) -> Self::Output {
+        let max_dim = cmp::max(self.degree(), other.degree());
+        let mut out_f: Vec<T> = vec![zero(); max_dim + 1];
+        for i in 0..self.degree()+1 { out_f[i] += self.f[i].clone() }
+        for i in 0..other.degree()+1 { out_f[i] += other.f[i].clone() }
+
+        Poly {
+            f: out_f,
+        }.reduce()
+    }
+} 
+
+impl<T: Num+std::clone::Clone+AddAssign> Add for Poly<T> {
+    type Output = Self;
     fn add(self, other: Self) -> Self {
         let max_dim = cmp::max(self.degree(), other.degree());
         let mut out_f: Vec<T> = vec![zero(); max_dim + 1];
-        for i in 0..self.degree()+1 { out_f[i] = out_f[i].clone() + self.f[i].clone() }
-        for i in 0..other.degree()+1 { out_f[i] = out_f[i].clone() + other.f[i].clone() }
+        for i in 0..self.degree()+1 { out_f[i] += self.f[i].clone() }
+        for i in 0..other.degree()+1 { out_f[i] += other.f[i].clone() }
 
         Self {
             f: out_f,
         }.reduce()
     }
-    
-
 } 
 
-impl<T: Add+Num+std::clone::Clone> Sub for Poly<T> {
+
+impl<T: Num+std::clone::Clone> Sub for Poly<T> {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
@@ -91,7 +132,7 @@ impl<T: Add+Num+std::clone::Clone> Sub for Poly<T> {
 
 }
 
-impl<T: Add+Num+std::clone::Clone> Mul for Poly<T> {
+impl<T: Num+std::clone::Clone> Mul for Poly<T> {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
@@ -109,8 +150,77 @@ impl<T: Add+Num+std::clone::Clone> Mul for Poly<T> {
     }
 }
 
+impl<T: Num+std::clone::Clone> MulAssign for Poly<T> {
+    // TODO refactor
+    fn mul_assign(&mut self, other: Self) {
+        let dim = self.degree() + other.degree();
+        let mut out_f: Vec<T> = vec![zero(); dim + 1];
+        for i in 0..self.degree()+1 { 
+            for j in 0..other.degree()+1 {
+                out_f[i+j] = out_f[i+j].clone() + self.f[i].clone() * other.f[j].clone()
+            }
+        }
+        (*self).f = out_f;
+    }
+}
 
-impl<T: Add+Num+std::clone::Clone>  Poly<T> {
+impl<T: Num+std::clone::Clone+std::fmt::Debug> Div for Poly<T> {
+    type Output = Self;
+
+    // otherはmonicとする.
+    fn  div(self, other: Self) -> Self {
+        assert_eq!(other.f[other.degree()], one());
+        let mut rem = self.clone();
+        let self_d = self.degree() as i32;
+        let other_d = other.degree() as i32;
+        let deg = self_d - other_d;
+        if deg < 0 {return Poly{f: vec![zero(); 1]}};
+        let mut divided = Poly{f: vec![zero(); deg as usize+1]};
+        for i in 0..deg+1 {
+            // あまりの係数
+            let target_index = (deg - i + other_d) as usize;
+            let coeff = rem.f[target_index].clone();
+            // あまりの次数
+            let divided_index = (deg - i + 1) as usize;
+            let mut _f = vec![zero(); divided_index];
+            _f[divided_index-1] = coeff.clone();
+            rem = rem - Poly{f: _f} * other.clone();
+            divided.f[(deg-i) as usize] = coeff;
+        }
+        divided
+
+    }
+}
+
+impl<T: Num+std::clone::Clone+std::fmt::Debug> Rem for Poly<T> {
+    type Output = Self;
+
+    // otherはmonicとする.
+    fn  rem(self, other: Self) -> Self {
+        assert_eq!(other.f[other.degree()], one());
+        let mut rem = self.clone();
+        let self_d = self.degree() as i32;
+        let other_d = other.degree() as i32;
+        let deg = self_d - other_d;
+        if deg < 0 {return Poly{f: vec![zero(); 1]}};
+        let mut divided = Poly{f: vec![zero(); deg as usize+1]};
+        for i in 0..deg+1 {
+            // あまりの係数
+            let target_index = (deg - i + other_d) as usize;
+            let coeff = rem.f[target_index].clone();
+            // あまりの次数
+            let divided_index = (deg - i + 1) as usize;
+            let mut _f = vec![zero(); divided_index];
+            _f[divided_index-1] = coeff.clone();
+            rem = rem - Poly{f: _f} * other.clone();
+            divided.f[(deg-i) as usize] = coeff;
+        }
+        rem
+    }
+
+}
+
+impl<T: Num+std::clone::Clone>  Poly<T> {
     pub fn scalar(&mut self, a: T) -> Poly<T> {
         let a_poly = Poly{f: vec![a]};
         Poly{f: self.f.clone() } * a_poly
@@ -120,4 +230,77 @@ impl<T: Add+Num+std::clone::Clone>  Poly<T> {
          _f.reverse();
          _f.iter().cloned().fold(Zero::zero(), |sub, i| sub * a.clone() + i)
     }
+}
+
+impl<T: Num+std::clone::Clone+AddAssign>  Poly<T> {
+    pub fn power(&self, p: usize) -> Poly<T> {
+        let mut out_poly = self.clone();
+        for _ in 0..p-1 {out_poly *= self.clone()};
+        out_poly
+    }
+
+    pub fn comp(&self, other: &Self) -> Self {
+        // gのn乗をループで計算
+        // n乗ごとにfの定数倍を出す
+        let dim = self.degree() * other.degree();
+        let mut composed = Poly{f: vec![zero(); dim + 1]};
+        let mut other_power = other.clone();
+        composed.f[0] = self.f[0].clone();
+        for i in 1..self.degree()+1 {
+            let coeff = Poly{f: vec![self.f[i].clone()]};
+            composed += coeff * other_power.clone();
+            other_power = other_power * other.clone();
+        };
+        composed
+    }
+
+    pub fn df(&self) -> Self {
+        let dim = self.degree();
+        let mut df_f = vec![];
+        let mut j = T::zero();
+        for i in 1..dim+1 {
+            j += one();
+            df_f.push(j.clone() * self.f[i].clone())
+        }
+        Poly{
+            f: df_f
+        }
+
+    }
+
+}
+
+// rational Polynomialの場合の話
+impl Poly<Rational> {
+  pub fn gcd(&self, other: &Self) -> Poly<Rational> {
+      let mut f = self.clone();
+      let mut g = other.clone();
+      if self.degree() < other.degree() {
+          f = other.clone();
+          g = self.clone();
+      };
+      while g.degree() > 0 || g.f[0] != zero() {
+          let _f = f.clone().rem(g.monic());
+          f = g;
+          g = _f;
+      }
+      f.monic()
+  }
+
+  pub fn monic(&self) -> Poly<Rational> {
+      let deg = self.degree();
+      let mut out_f = vec![];
+      let coeff = self.f[deg];
+      for i in 0..deg+1 {
+          out_f.push(self.f[i] / coeff)
+      };
+      Poly{
+          f: out_f
+      }
+  }
+
+  pub fn squarefree(&self) -> Poly<Rational> {
+      let df = self.df();
+      self.clone() / self.gcd(&df)
+  }
 }
